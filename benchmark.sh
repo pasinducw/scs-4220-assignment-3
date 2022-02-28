2e76aa99a389f3ac8a73f88037e8e3c99f6f5f83
Pasindu Wijesena <pasindu@the-ai.team>
2022-03-01 01:07:55 +0530

    feat(benchmark): benchmarking tool

---
+++ benchmark.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "N;Concurrent;Sequential;"
+
+for rep in {1..20}
+  do
+    for limit in 10 50 100 500 1000 5000 10000 50000 100000 500000 1000000 5000000 10000000 50000000 100000000
+      do
+        concurrent=$(scala3 -J-Xmx32g ConcurrentApplication.scala ${limit} | grep Time | grep -Eo '[0-9][0-9]*')
+        sequential=$(scala3 -J-Xmx32g SequentialApplication.scala ${limit} | grep Time | grep -Eo '[0-9][0-9]*')
+        echo "$limit;$concurrent;$sequential;"
+    done
+done
